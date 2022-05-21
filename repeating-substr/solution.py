4e8b951b688675714f176d6ba42ee551ac0243ba
Matthew Falcioni <mfalcioni1@gmail.com>
2022-05-20 20:48:14 -0400

    done

--- repeating-substr/solution.py
+++ repeating-substr/solution.py
@@ -1,20 +1,29 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        def matchChar(s, idx, n_l):
+        def matchChar(s, idx, u_seq):
             if idx < len(s):
-                if s[idx] != s[idx+1]:
-                    matchChar(s, idx+1, n_l+1)
-                else:
-                    return n_l
-        m_l = 1 # min max length
-        for i in range(len(s)):
-            n_l = matchChar(s[i:len(s)-1], 0, 1)
-            m_l = max(n_l, m_l)
+                if s[idx] not in u_seq:
+                    u_seq = u_seq + s[idx]
+                    return matchChar(s, idx + 1, u_seq)
+            return len(u_seq)
+        if len(s) == 0:
+            return 0
+        else:
+            m_l = 1 # min max length
+        for i in range(len(s)): #skip empty and singular string 
+            sub_str = s[i:len(s)]
+            if m_l < len(sub_str):
+                n_l = matchChar(sub_str, 0, '')
+                m_l = max(n_l, m_l)
         return m_l
 
 
-
-
-s = "pwwkew"
 sol = Solution()
-sol.lengthOfLongestSubstring(s)
+s = "au"
+sol.lengthOfLongestSubstring(s) == 2
+s = "pwwkew"
+sol.lengthOfLongestSubstring(s) == 3
+s = "abcabcbb"
+sol.lengthOfLongestSubstring(s) == 3
+s = "bbbbb"
+sol.lengthOfLongestSubstring(s) == 1
