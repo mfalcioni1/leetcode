6ffd9d378927de5db1f2ee7314d546305d82ec42
matthew-falcioni <mfalcioni1@gmail.com>
2022-06-21 21:37:36 -0400

    oop question from shopify

---
+++ other-algorithms-qs/shopify.py
@@ -0,0 +1,42 @@
+import re
+
+class string_insert:
+    
+    def __init__(self, s, op, cursor = 0) -> None:
+        self.s = s
+        self.op = op
+        self.cursor = cursor
+
+    def R(self):
+        r_act = re.findall("R(.+)", self.op)[0]
+        return r_act
+    
+    def F(self):
+        f_ttl = re.findall("F(\d+)", self.op)
+        f_ttl = [int(x) for x in f_ttl]
+        return sum(f_ttl)
+    
+    def B(self):
+        b_ttl = re.findall("B(\d+)", self.op)
+        b_ttl = [int(x) for x in b_ttl]
+        return sum(b_ttl)
+    
+    def insert(self):
+        with_char = self.R()
+        self.cursor = self.F() - self.B()
+        print(self.s[self.cursor])
+        replace_char = self.s[self.cursor]
+        res = self.s.replace(replace_char, with_char)
+        return res
+        
+
+
+
+op = "F2B1F5Rw"
+s = "abcdefghijklmn"
+solution = string_insert(s, op)
+
+solution.insert()
+
+# expected result
+"abcdefwhijklmn" == solution.insert()
