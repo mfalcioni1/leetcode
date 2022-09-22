130828acf0a557ff3bdd6b03f9aa3d5152704385
Matt Falcioni <mfalcioni1@gmail.com>
2022-09-21 22:07:11 -0400

    trying to add user's dumb feature requests

--- other-algorithms-qs/sim_comet_stellar_pup.py
+++ other-algorithms-qs/sim_comet_stellar_pup.py
@@ -2,29 +2,35 @@ import random
 def roll():
     return random.randint(1,6)
 
-def activate(damage: int, counter: int, activations: int):
+def activate(life: int, counter: int, blockers: int):
     r = roll()
     if counter <= 0:
-        return damage, counter, activations
+        return life, counter, blockers
     if r <= 2:
-        return damage + 2, counter + 2, activations + 1
+        return life, counter + 2, blockers - 2
     elif r <= 3:
-        return damage, counter - 1, activations + 1
+        return life, counter - 1, blockers
     elif r <=5:
-        return damage + counter, counter - 2, activations + 1
+        return life - counter, counter - 2, blockers
     else:
-        damage, counter, activations = activate(damage, counter + 1, activations + 1)
-        return activate(damage, counter, activations)
+        life, counter, blockers = activate(life, counter + 1, blockers)
+        return activate(life, counter, blockers)
 
-def main(iters):
-    results = {"damage": [], "counter": [], "activations": []}
+def main(iters: int, life: int, blockers: int):
+    results = {"life": [], "counter": [], "blockers": [], "win": []}
     for _ in range(iters):
-        d, c, a = activate(0, 5, 0)
-        results["damage"].append(d)
+        l, c, b = activate(life, 5, blockers)
         results["counter"].append(c)
-        results["activations"].append(a)
+        results["blockers"].append(b)
+        if blockers < 0:
+            life+=blockers
+        if life <= 0:
+            results["win"].append("yes")
+        else:
+            results["win"].append("no")
+        results["life"].append(l)
     return results
 
 n = 100000
-r = main(n)
-sum([i >= 20 for i in r["damage"]])/(n/100)
+r = main(n, 20, 0)
+sum([i <= 0 for i in r["life"]])/(n/100)
